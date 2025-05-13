9b2363d7293924477c61ab492a15667149036d8d
Teddy <ted.j.gallion@gmail.com>
2025-05-13 16:05:36 -0500

    testing backend connect with db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-
+const DB_URL = process.env.DATABASE_URL || "No db url set"
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
@@ -11,5 +11,12 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
-
+console.log('DB config in use:', {
+  PGUSER: process.env.PGUSER,
+  PGHOST: process.env.PGHOST,
+  PGDATABASE: process.env.PGDATABASE,
+  PGPORT: process.env.PGPORT,
+  PGSSLMODE: process.env.PGSSLMODE,
+});
+console.log(DB_URL)
 module.exports = pool;
